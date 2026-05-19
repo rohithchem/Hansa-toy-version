da8c4f8b2f53f3baf40244ef8418f16ae4a7315e
rohithchem <b.a.saieshwari@gmail.com>
2026-05-19 23:44:14 +0530

    Update and rename README.md to hansa_toy version.py

---
+++ hansa_toy version.py
@@ -0,0 +1,151 @@
+seq1 = "MDCSAPKEMNKLPANSPEAAAAQGHPDGPCAPRTSPEQELPAAAAPPPPRVPRSASTGAQTFQSADARACEAERPGVGSCKLSSPRAQAASAALRDLREAQGAQASPPPGSSGPGNALHCKIPFLRGPEGDANVSVGKGTLERNNTPVVGWVNMSQSTVVLATDGITSVLPGSVATVATQEDEQGDENKARGNWSSKLDFILSMVGYAVGLGNVWRFPYLAFQNGGGAFLIPYLMMLALAGLPIFFLEVSLGQFASQGPVSVWKAIPALQGCGIAMLIISVLIAIYYNVIICYTLFYLFASFVSVLPWGSCNNPWNTPECKDKTKLLLDSCVISDHPKIQIKNSTFCMTAYPNVTMVNFTSQANKTFVSGSEEYFKYFVLKISAGIEYPGEIRWPLALCLFLAWVIVYASLAKGIKTSGKVVYFTATFPYVVLVILLIRGVTLPGAGAGIWYFITPKWEKLTDATVWKDAATQIFFSLSAAWGGLITLSSYNKFHNNCYRDTLIVTCTNSATSIFAGFVIFSVIGFMANERKVNIENVADQGPGIAFVVYPEALTRLPLSPFWAIIFFLMLLTLGLDTMFATIETIVTSISDEFPKYLRTHKPVFTLGCCICFFIMGFPMITQGGIYMFQLVDTYAASYALVIIAIFELVGISYVYGLQRFCEDIEMMIGFQPNIFWKVCWAFVTPTILTFILCFSFYQWEPMTYGSYRYPNWSMVLGWLMLACSVIWIPIMFVIKMHLAPGRFIERLKLVCSPQPDWGPFLAQHRGERYKNMIDPLGTSSLGLKLPVKDLELGTQC"  #Q9Y345 Uniprot Id
+seq2 = "MDCSAPKEMNKQPANILEAAVPGHRDSPRAPRTSPEQDLPAEAPAATVQPPRVPRSASTGAQTFQSADARACEAQQSGVGFCNLSSPRAQATSAALRDLSEGHSAQANPPSGPAGAGNALHCKIPALRGPEEDANVSVGKGTLEHNNTPAVGWVNMSQSTVVLGTDGIASVLPGSVATTTIPEDEQGDENKARGNWSSKLDFILSMVGYAVGLGNVWRFPYLAFQNGGGAFLIPYLMMLALAGLPIFFLEVSLGQFASQGPVSVWKAIPALQGCGIAMLIISVLIAIYYNVIICYTLFYLFASFVSVLPWGSCNNPWNTPECKDKTKLLLDSCVIGDHPKIQIKNSTFCMTAYPNLTMVNFTSQTNKTFVSGSEEYFKYFVLKISAGIEYPGEIRWPLAFCLFLAWVIVYASLAKGIKSSGKVVYFTATFPYVVLVILLIRGVTLPGAGAGIWYFITPKWEKLTDATVWKDAATQIFFSLSAAWGGLITLSSYNKFHNNCYRDTLIVTCTNSATSIFAGFVIFSVIGFMANERKVNIENVADQGPGIAFVVYPEALTRLPLSPFWAIIFFLMLLTLGLDTMFATIETIVTSISDEFPKYLRTHKPVFTLGCCICFFIMGFPMITQGGIYMFQLVDTYAASYALVIIAIFELVGISYVYGLQRFCEDIEMMIGFKPNIFWKVCWAFVTPTILTFILCFSFYQWEPMTYGSYRYPNWSMVLGWLMLACSVIWIPIMFVIKMYLAPGRFIERLKLVCSPQPDWGPFLAQHRGERYKNMIDPLGTSSLGLKLPVKDLELGTQC" #mouse [BLAST aligner detected]
+seq3 = "MDCSAPKEMNKPPTNILEATVPGHRDSPRAPRTSPEQDLPAAAPAAAVQPPRVPRSASTGAQTFQSADARACEAQRPGVGFCKLSSPQAQATSAALRDLSEGHSAQANPPSGAAGAGNALHCKIPALRGPEEDENVSVGKGTLEHNNTPAVGWVNMSQSTVVLGTDGIASVLPGSVATTTIPEDEQGDENKARGNWSSKLDFILSMVGYAVGLGNVWRFPYLAFQNGGGAFLIPYLMMLALAGLPIFFLEVSLGQFASQGPVSVWKAIPALQGCGIAMLIISVLIAIYYNVIICYTLFYLFASFVSVLPWGSCNNPWNTPECKDKTKLLLDSCVIGDHPKIQIKNSTFCMTAYPNLTMVNFTSQANKTFVSGSEEYFKYFVLKISAGIEYPGEIRWPLAFCLFLAWVIVYASLAKGIKTSGKVVYFTATFPYVVLVILLIRGVTLPGAGAGIWYFITPKWEKLTDATVWKDAATQIFFSLSAAWGGLITLSSYNKFHNNCYRDTLIVTCTNSATSIFAGFVIFSVIGFMANERKVNIENVADQGPGIAFVVYPEALTRLPLSPFWAIIFFLMLLTLGLDTMFATIETIVTSISDEFPKYLRTHKPVFTLGCCICFFIMGFPMITQGGIYMFQLVDTYAASYALVIIAIFELVGISYVYGLQRFCEDIEMMIGFQPNIFWKVCWAFVTPTILTFILCFSFYQWEPMTYGSYRYPNWSMVLGWLMLACSVIWIPIMFVIKMYLAPGRFIERLKLVCSPQPDWGPFLAQHRGERYKNMIDPLGTSSLGLKLPVKDLELGTQC" #rat [BLAST aligner detected]
+seq4 = "MDCSAPKEMNKLPANSPEAAAAQGHPDGPCAPRTSPEQELPAAAAPPPPRVPRSASTGAQTFQSADARACEAERPGVGSCKLSSPRAQAASAALRDLREAQGAQASPPPGSSGPGNALHCKIPSLRGPEGDGNVSVGKGTLERNNTPVVGWVNMSQSTVVLATDGITSVLPGSVATVATQEDEQGDENKARGNWSSKLDFILSMVGYAVGLGNVWRFPYLAFQNGGGAFLIPYLMMLALAGLPIFFLEVSLGQFASQGPVSVWKAIPALQGCGIAMLIISVLIAIYYNVIICYTLFYLFASFVSVLPWGSCNNPWNTPECKDKTKLLLDSCVISDHPKIQIKNSTFCMTAYPNVTMVNFTSQANKTFVSGSEEYFKYFVLKISAGIEYPGEIRWPLALCLFLAWVIVYASLAKGIKTSGKVVYFTATFPYVVLVILLIRGVTLPGAGAGIWYFITPNWEKLTNATVWKDAATQIFFSLSAAWGGLITLSSYNKFHNNCFRDTLIVTCTNSATSIFAGFVIFSVIGFMANERKVNIENVADQGPGIAFVVYPEALTRLPLSPFWAIIFFLMLLTLGLDTMFATIETIVTSISDEFPKYLRTHKPVFTLGCCICFFIMGFPMITQGGIYMFQLVDTYAASYALVIIAIFELVGISYVYGLQRFCEDIEMMIGFQPNIFWKVCWAFVTPTILTFILCFSFYQWEPMTYGSYRYPNWSMVLGWLMLACSVIWIPIMFVIKMHLAPGRFIERLKLVCSPQPDWGPFLAQHRRERYKNMIDPLGTSSLGLKLPVKDLELGTQC"  #neutral mutations inserted 
+seq5 = "MDCSAPKEMNKLPANSPEAAAAQGHPDGPCAPRTSPEQELPAAAAPPPPRVPRSASTGAQTFQSADARACEAERPGVGSCKLSSPRAQAASAALRDLREAQGAQASPPPGSSGPGNALHCKIPFLRGPEGDANVSVGKGTLERNNTPVVGWVNMSQSTVVLATDGITSVLPGSVATVATQEDEQGDENKARGNWSSKLDFILSMVGYAVGLGNVWRFPYLAFQNGGGAFLIPYLMMLALAGLPIFFLEVSLGQFASQGPVSVWKAIPALQGCGIAMLIISVLIAIYYNVIICYTLFYLFASFVSVVPWGSCNNPWNTPECKDKTKLLLDSCVISDHPKIQIKNSTFCMTAYPNVTMVNFTSQANKTFVSGSEEYFKYFVLKISAGIEYPGEIRWPLALCLFLAWVIVYASLAKGIKTSGKVVYFMATFPYVVLVILLIRGVTLPGAGAGIWYFITPKWEKLTDATVWKDAATQIFFSLSAACGGLITLSSCNKFHNNCYRDTLIVTCTSRATSIFAGFVIFSVIGFMANERKVNIENVADQGPGIAFVVYPEALTRLPLSPFWAIIFFLMLLTLGLDTMFATIETIVTSISDEFPKYLRTHKPVFTLGCCICFFIMGFPMITQGGIYMFQLVDTYAASYALVIIAIFELVGISYVYGLQRFCEDIEMMIGFQPNIFWKVCWAFVTPTILTFILCFSFYQWEPMTYGSYRYPNWSMVLGWLMLACSVIWIPIMFVIKMHLAPGRFIERLKLVCSPQPDWGPFLAQHRGERYKNMIDPLGTSSLGLKLPVKDLELGTQC"  #P02088
+sequences = [seq1, seq2, seq3, seq4, seq5]
+mutations_neutral = [
+    (101, "G", "S"), (123, "F", "S"), (131, "A", "G"), (456, "K", "N"), (462, "D", "N"), (498, "Y", "F"), (766, "G", "R")    
+]
+mutations_deleterious = [
+    (305, "L", "V"), (424, "T", "M"), (481, "W", "C"), (490, "Y", "C"), (508, "N", "S"), (509, "S", "R")                     
+]
+#buried or exposed with 10 % cutoff as used in hansa paper [0 if exposed, 1 if buried], position number[0 indexing] : 0/1 format
+acess = {101 : 0, 123 : 0, 131 : 0, 456 : 0, 462 : 0, 498 : 0, 766 : 0, 306 : 0, 425 : 1, 481 : 0, 490 : 0, 508 : 1, 509 : 1}
+#strucutre : 1 if part of alpha helix, 2 if part of extended strand and 0 for others
+strct = {101 : 0, 123 : 0, 131 : 0, 456 : 0, 462 : 0, 498 : 1, 766 : 0, 306 : 0, 425 : 1, 481 : 0, 490 : 0, 508 : 1, 509 : 1}
+#the above buried/exposed and strucutre data was obtained from "https://services.healthtech.dtu.dk/cgi-bin/webface2.cgi?jobid=6A0C46C300279D39CD285359&wait=20
+delet = []
+neut = []
+wild = []
+for num,wildtype, mutation in mutations_neutral:
+    neut.append(mutation)
+    wild.append(wildtype)
+for num,wildtype, mutation in mutations_deleterious:
+    delet.append(mutation)
+    wild.append(wildtype)
+
+def Pwild_type(pos):
+    amino_acids = []
+    P_Wt = {}
+    for seq in sequences:
+        amino_acids.append(seq[pos])
+    for acid in amino_acids:
+        if acid in wild:
+            P_Wt[acid] = amino_acids.count(acid)/len(amino_acids)
+    return P_Wt    #amino acid : number
+def Pmutation(pos):
+    amino_acids = []
+    P_Mut = {}
+    for seq in sequences:
+        amino_acids.append(seq[pos])
+    for acid in amino_acids:
+        if acid in neut or delet:
+            P_Mut[acid] = amino_acids.count(acid)/len(amino_acids)
+    return P_Mut      #amino acid : number
+
+def diff_bw_P(pos, res):   #here the res is the wild type
+    wildn = Pwild_type(pos)
+    mutat = Pmutation(pos)
+    if res in wildn:
+        req_wd = wildn[res]
+    else:
+        req_wd = 0
+    req_m = 0
+    for num, wil, mut in(mutations_neutral + mutations_deleterious):
+        if wil == res:
+            req_m = mutat.get(mut, 0)
+        else:
+            req_m = 0
+    return req_wd - req_m
+#return P(wild_type) - P(mutations) for given position and given wild type - mutation pair
+
+import pandas as pd
+matrix = [          #BLOSSUM62 Matrix 
+[4,-1,-2,-2,0,-1,-1,0,-2,-1,-1,-1,-1,-2,-1,1,0,-3,-2,0],
+[-1,5,0,-2,-3,1,0,-2,0,-3,-2,2,-1,-3,-2,-1,-1,-3,-2,-3],
+[-2,0,6,1,-3,0,0,0,1,-3,-3,0,-2,-3,-2,1,0,-4,-2,-3],
+[-2,-2,1,6,-3,0,2,-1,-1,-3,-4,-1,-3,-3,-1,0,-1,-4,-3,-3],
+[0,-3,-3,-3,9,-3,-4,-3,-3,-1,-1,-3,-1,-2,-3,-1,-1,-2,-2,-1],
+[-1,1,0,0,-3,5,2,-2,0,-3,-2,1,0,-3,-1,0,-1,-2,-1,-2],
+[-1,0,0,2,-4,2,5,-2,0,-3,-3,1,-2,-3,-1,0,-1,-3,-2,-2],
+[0,-2,0,-1,-3,-2,-2,6,-2,-4,-4,-2,-3,-3,-2,0,-2,-2,-3,-3],
+[-2,0,1,-1,-3,0,0,-2,8,-3,-3,-1,-2,-1,-2,-1,-2,-2,2,-3],
+[-1,-3,-3,-3,-1,-3,-3,-4,-3,4,2,-3,1,0,-3,-2,-1,-3,-1,3],
+[-1,-2,-3,-4,-1,-2,-3,-4,-3,2,4,-2,2,0,-3,-2,-1,-2,-1,1],
+[-1,2,0,-1,-3,1,1,-2,-1,-3,-2,5,-1,-3,-1,0,-1,-3,-2,-2],
+[-1,-1,-2,-3,-1,0,-2,-3,-2,1,2,-1,5,0,-2,-1,-1,-1,-1,1],
+[-2,-3,-3,-3,-2,-3,-3,-3,-1,0,0,-3,0,6,-4,-2,-2,1,3,-1],
+[-1,-2,-2,-1,-3,-1,-1,-2,-2,-3,-3,-1,-2,-4,7,-1,-1,-4,-3,-2],
+[1,-1,1,0,-1,0,0,0,-1,-2,-2,0,-1,-2,-1,4,1,-3,-2,-2],
+[0,-1,0,-1,-1,-1,-1,-2,-2,-1,-1,-1,-1,-2,-1,1,5,-2,-2,0],
+[-3,-3,-4,-4,-2,-2,-3,-2,-2,-3,-2,-3,-1,1,-4,-3,-2,11,2,-3],
+[-2,-2,-2,-3,-2,-1,-2,-3,2,-1,-1,-2,-1,3,-3,-2,-2,2,7,-1],
+[0,-3,-3,-3,-1,-2,-2,-3,-3,3,1,-2,1,-1,-2,-2,0,-3,-1,4],
+]
+rows = ["A","R","N","D","C","Q","E","G","H","I","L","K","M","F","P","S","T","W","Y","V"]
+cols = ["A","R","N","D","C","Q","E","G","H","I","L","K","M","F","P","S","T","W","Y","V"]
+
+BLOSUM62 = pd.DataFrame(matrix, index = rows, columns = cols)
+
+def gribskov(pos, acid):             #here we can choose whichever acid we want (either wild type or mutant)
+    seqs = [seq1, seq2, seq3, seq4, seq5]
+    aacids = []
+    for seq in seqs:
+        aacids.append(seq[pos])
+    score = 0
+    for amacid in aacids:
+        score += BLOSUM62[amacid][acid]
+    return score
+
+#using gribskov() function, we can calculate score for both wild type and mutant and subtract also
+
+
+#7 neutral mutations + 6 deleterious mutations so we find for 13 mutations
+data = {}
+for pos, wldtp, new in (mutations_neutral + mutations_deleterious):
+    for pos, binaryval1 in acess.items():
+        for pos, binaryval2 in strct.items():
+            data[pos] = [Pwild_type(pos), Pmutation(pos), diff_bw_P(pos, wldtp), gribskov(pos, wldtp), acess[pos], strct[pos]]
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
+df = pd.DataFrame.from_dict(data, orient="index")
+df = df.reset_index()
+df.columns = ["position", "wild_type", "mutation", "diff", "score", "acess", "strct"]
+
+
+
+#took the output, and converted into a numeric vector, therefore representing each vector position usign numeric values
+
+
+x = [
+[0,0,1.0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1.0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.0,0,0,0],  
+[0.2,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.4, 0.2, -4, 0, 0],  
+[0,0,0.2,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0.2,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0.4,0, 0.0, 2,     0, 0],  
+[0,0,0,0.2,0,0,0,0,0.4,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0.2,0,0,0,0,0.4,0,0,0,0,0,0,0,0.4,0,0, 0.0,  3, 0, 0],  
+[0,0,0,0.2,0,0,0,0,0,0,0,0,0,0,0.4,0.4,0,0,0,0,0,0,0.2,0,0,0,0,0,0,0,0,0,0,0.4,0.4,0,0,0, 0.0,     -3, 0, 0],  
+[0,0.2,0,0.4,0,0,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0,0.2,0,0.4,0,0,0,0,0,0.4,0,0,0,0,0,0,0,0,0, 0.0,      -4, 0, 1], 
+[0,0,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.4,0,0.2,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0, -0.2, -3,      0, 0],  
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0,0.6,0,0,0,0,0,0, 0.0, -7, 0, 0],  
+[0,0.4,0,0,0,0,0,0,0,0,0,0.6,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0.6,0,0,0,0,0,0,0, 0.0, -1, 1,1],  
+[0,0,0,0,0,0,0,0,0,0,0.4,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0,0,0.4,0.4,0,0,0,0,0,0,0, 0.0, -5,     0, 0],  
+[0.4,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0,0.2,0,0.4,0,0,0,0,0,0,0,0,0, 0.4, 3,     0, 0],  
+[0.2,0,0,0.4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.2,0,0,0.4,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0,0, 0.2,       4, 1, 1],  
+[0.4,0,0,0,0,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0,0.4,0,0,0,0.2,0,0,0,0.4,0,0,0,0,0,0,0,0,0,0, 0.2, 9,       1, 1]  
+]
+
+
+import numpy as np
+x_data = np.array(x)
+
+
+#taking neutral as 1, and deleterious as 0, we get y = (1,1,1,1,1,1,1,0,0,0,0,0,0)
+y = (1,1,1,1,1,1,1,0,0,0,0,0,0)
+y_data = np.array(y)
+from sklearn.svm import SVC
+model = SVC(kernel = 'rbf', C = 1.0)
+model.fit(x_data, y_data)
+pred = model.predict(x_data)
+print(pred)
+
